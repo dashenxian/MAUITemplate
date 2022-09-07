5acc17013d4e905368dd0fc67ebce216c7f5072e
1747623132@qq.com <1747623132@qq.com>
2022-09-07 14:58:53 +0800

    添加项目文件。

---
+++ MAUITemplate/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUITemplate.Models
+{
+    public abstract class Entity<TKey>: IEntity<TKey>
+    {
+        /// <summary>
+        /// 主键id
+        /// </summary>
+        public TKey Id { get; set; }
+    }
+}
---
+++ MAUITemplate/Models/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUITemplate.Models
+{
+    /// <summary>
+    /// 类型接口
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IEntity<TKey>
+    {
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        TKey Id { get; set; }
+    }
+}
